ea9fe7c071cd541acebbc39f39005e35fdd67dd7
Kaibin <diaokaibin@gmail.com>
2021-02-09 10:18:21 +0800

    update Hi

--- app/src/main/java/com/one/bee/demo/tab/HiTabTopActivity.kt
+++ app/src/main/java/com/one/bee/demo/tab/HiTabTopActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.one.bee.R
-import com.one.ui.tab.common.IHiTabLayout
 import com.one.ui.tab.top.HiTabTopInfo
 import com.one.ui.tab.top.HiTabTopLayout
 import java.util.*
