3a9b782eb8e3dc4757c13751fcb7a570bed3ffc0
Kaibin <diaokaibin@gmail.com>
2021-02-09 11:17:37 +0800

    update Hi

---
+++ app/src/main/java/com/one/bee/fragment/CategoryFragment.java
@@ -0,0 +1,15 @@
+package com.one.bee.fragment;
+
+import com.one.bee.R;
+import com.one.common.ui.component.HiBaseFragment;
+
+/**
+ * @author diaokaibin@gmail.com on 2/9/21.
+ */
+public class CategoryFragment extends HiBaseFragment {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_category;
+    }
+}
---
+++ app/src/main/java/com/one/bee/fragment/HomePageFragment.java
@@ -0,0 +1,15 @@
+package com.one.bee.fragment;
+
+import com.one.bee.R;
+import com.one.common.ui.component.HiBaseFragment;
+
+/**
+ * @author diaokaibin@gmail.com on 2/9/21.
+ */
+public class HomePageFragment extends HiBaseFragment {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_home;
+    }
+}
