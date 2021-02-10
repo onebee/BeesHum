69dde8dae9896f642f41463c4068120b9c98c336
Kaibin <diaokaibin@gmail.com>
2021-02-10 20:28:30 +0800

    update HiDateItem

--- app/src/main/java/com/one/bee/fragment/HomePageFragment.java
+++ app/src/main/java/com/one/bee/fragment/HomePageFragment.java
@@ -2,15 +2,21 @@ package com.one.bee.fragment;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
 
 import com.one.bee.R;
 import com.one.bee.demo.banner.HiBannerDemoActivity;
 import com.one.common.ui.component.HiBaseFragment;
+import com.one.library.log.HiConsolePrinter;
+import com.one.library.log.HiLog;
+import com.one.library.log.HiLogManager;
 
 /**
  * @author diaokaibin@gmail.com on 2/9/21.
  */
 public class HomePageFragment extends HiBaseFragment {
+
+    TextView tv;
 
     @Override
     public int getLayoutId() {
@@ -21,12 +27,28 @@ public class HomePageFragment extends HiBaseFragment {
     @Override
     public void onStart() {
         super.onStart();
-
-        getActivity().findViewById(R.id.tv_home).setOnClickListener(new View.OnClickListener() {
+        HiConsolePrinter hiConsolePrinter = new HiConsolePrinter();
+        HiLogManager.getInstance().addPrinter(hiConsolePrinter);
+       tv= getActivity().findViewById(R.id.tv_home);
+               tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), HiBannerDemoActivity.class));
 
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        HiLog.i(" onResume : " + tv.getWidth() + " , " + tv.getHeight());
+
+        tv.post(new Runnable() {
+            @Override
+            public void run() {
+                HiLog.i(" post onResume : " + tv.getWidth() + " , " + tv.getHeight());
             }
         });
     }
